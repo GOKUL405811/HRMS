263cbcf671d8f448dcd2202e83e9fdb1d4fd3965
GOKUL405811 <gokulkrishnabnair@gmail.com>
2025-12-03 20:39:45 +0530

    Update settings and requirements with Brevo SMTP

--- HR_management_systems/HR_management_systems/settings.py
+++ HR_management_systems/HR_management_systems/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 import pymysql
+load_dotenv()
 pymysql.install_as_MySQLdb()
 
 
@@ -161,13 +162,21 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 DEFAULT_ADMIN_EMAIL = "gokulkrishnabnair@gmail.com"
 DEFAULT_ADMIN_PASSWORD = "Gokul@1234"
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = "smtp.gmail.com"
+from dotenv import load_dotenv
+import os
+
+
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp-relay.brevo.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "gokulkrishnabnair@gmail.com"
-EMAIL_HOST_PASSWORD = "rvagrxabwlusqybo"
+
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
 
 USE_TZ = False
 TIME_ZONE = 'Asia/Kolkata'
